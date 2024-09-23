bb762f1eaa231835369e77df61f9120adea44e59
Sid. S <siddharth.shailesh@icloud.com>
2024-09-23 15:10:07 -0400

    Test Drive Train

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
@@ -1,130 +1,100 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
-// Akshay's edit
-
-
-@TeleOp()
-
-// “MecanumDriveTrain” should be the name of the file!
-
-public class MecanumDriveTrain extends OpMode {
-
-    // RF stands for Right Front
-    // LB stands for Left Back
-
-    // boolean wasDown, wasUp;
-    boolean flag_right_trigger = true;
-    boolean LinearSlideForwardTelemetry, LinearSlideReversedTelemetry = true;
-
-
-    DcMotor RFMotor;
-    DcMotor RBMotor;
-    DcMotor LFMotor;
-    DcMotor LBMotor;
-
-    /*
-
-    DcMotor leftLinearSlide;
-    DcMotor rightLinearSlide;
-
-    */
-
-
-    private Servo clawServo1;
-    private Servo clawServo2;
+@TeleOp
+public class MecanumDriveTrain extends LinearOpMode {
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
-        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
-        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
-        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
-        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+        Servo clawServo1 = hardwareMap.get(Servo.class, "ClawServo1");
+        Servo clawServo2 = hardwareMap.get(Servo.class, "ClawServo2");
 
-        clawServo1 = hardwareMap.get(Servo.class, "clawServo1");
-        clawServo2 = hardwareMap.get(Servo.class, "clawServo2");
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
 
-        /*
+        waitForStart();
 
-        leftLinearSlide = hardwareMap.get(DcMotor.class, "leftLinearSlide");
-        rightLinearSlide = hardwareMap.get(DcMotor.class, "rightLinearSlide");
+        while (opModeIsActive()) {
 
-         */
+            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = -gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
 
-        // This may have to be changed or deleted based on the Mecanum wheel orientation
+            if (gamepad1.right_bumper) {
+                telemetry.addData("Right Claw:", "Activated");
+                clawServo1.setPosition(0.3);
 
-        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            }
+
+            if (gamepad1.left_bumper){
+                telemetry.addData("Left Claw:", "Activated");
+                clawServo2.setPosition(0.3);
+
+            }
+
+            if (!gamepad1.left_bumper && !gamepad1.right_bumper) {
+                telemetry.addData("Both Claws:", "Deactivated");
+                clawServo1.setPosition(0);
+                clawServo2.setPosition(0);
+            }
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
     }
-
-    @Override
-    public void loop() {
-
-
-        double vertical = -gamepad1.left_stick_y;
-        double horizontal = gamepad1.left_stick_x * 1.1;
-        double pivot = gamepad1.right_stick_x;
-
-        float clawServoPosition = gamepad1.right_trigger;
-
-        double denominator = Math.max(Math.abs(vertical) + Math.abs(horizontal) + Math.abs(pivot), 1);
-
-        RFMotor.setPower((pivot + (-vertical + horizontal)) / denominator);
-        RBMotor.setPower((pivot + (-vertical - horizontal)) / denominator);
-        LFMotor.setPower((pivot + (-vertical - horizontal)) / denominator);
-        LBMotor.setPower((pivot + (-vertical + horizontal)) / denominator);
-
-        // Assuming that both Claw Servo's are Standard Servo's
-
-        if (gamepad1.right_trigger >= 0.5) {
-
-            telemetry.addData("Claw", "Open");
-            clawServo1.setPosition(0.6);
-            // clawServo2.setPosition(0.6);
-
-        }
-
-        else {
-
-            telemetry.addData("Claw", "Close");
-            clawServo1.setPosition(0);
-            // clawServo2.setPosition(0);
-
-        }
-
-
-
-        /*
-
-        double linearSlidePower = 0.3;
-
-
-        if (gamepad1.dpad_up) {
-            telemetry.addData("Left and Right Linear Slide", "Activated (Forward)");
-            leftLinearSlide.setPower(linearSlidePower);
-            rightLinearSlide.setPower(linearSlidePower);
-        }
-
-        else if (gamepad1.dpad_down) {
-            telemetry.addData("Left and Right Linear Slide", "Activated (Reverse)");
-            leftLinearSlide.setPower(-linearSlidePower);
-            rightLinearSlide.setPower(-linearSlidePower);
-        }
-        else {
-            leftLinearSlide.setPower(0);
-            rightLinearSlide.setPower(0);
-        }
-
-         */
-    }
-
 }
 
