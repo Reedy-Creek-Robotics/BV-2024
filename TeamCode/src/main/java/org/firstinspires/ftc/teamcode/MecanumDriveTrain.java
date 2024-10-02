86602e952e5ca4b208241ad50125eb80d3aa8a51
Sid. S <siddharth.shailesh@icloud.com>
2024-10-02 17:53:42 -0400

    LinearSlides Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
@@ -2,12 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
+
+import java.security.cert.LDAPCertStoreParameters;
+import java.util.ArrayList;
+import java.util.List;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
 
 @TeleOp
 public class MecanumDriveTrain extends LinearOpMode {
@@ -24,6 +31,13 @@ public class MecanumDriveTrain extends LinearOpMode {
         Servo clawServo1 = hardwareMap.get(Servo.class, "ClawServo1");
         Servo clawServo2 = hardwareMap.get(Servo.class, "ClawServo2");
 
+        ElapsedTime timeSinceRightBumperPressed = new ElapsedTime();
+
+        boolean claw_state = false; // false as open, true as closed
+
+        double claw1_position = clawServo1.getPosition();
+        double claw2_position = clawServo2.getPosition();
+
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
@@ -35,41 +49,40 @@ public class MecanumDriveTrain extends LinearOpMode {
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
-
         waitForStart();
 
         while (opModeIsActive()) {
 
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = -gamepad1.left_stick_x;
-            double rx = -gamepad1.right_stick_x;
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
-            if (gamepad1.right_bumper) {
-                telemetry.addData("Right Claw:", "Activated");
-                clawServo1.setPosition(0.3);
+
+
+
+            if(gamepad1.right_bumper) {
+                if(timeSinceRightBumperPressed.milliseconds() > 500) {
+                    timeSinceRightBumperPressed.reset();
+                    claw_state = !claw_state;
+
+                }
+
 
             }
 
-            if (gamepad1.left_bumper){
-                telemetry.addData("Left Claw:", "Activated");
-                clawServo2.setPosition(0.3);
-
+            if(claw_state) {
+              clawServo1.setPosition(claw1_position - 0.3);
+              clawServo2.setPosition(claw2_position + 0.3);
+            }
+            else {
+              clawServo1.setPosition(claw1_position);
+              clawServo2.setPosition(claw2_position);
             }
 
-            if (!gamepad1.left_bumper && !gamepad1.right_bumper) {
-                telemetry.addData("Both Claws:", "Deactivated");
-                clawServo1.setPosition(0);
-                clawServo2.setPosition(0);
-                // Trust
-            }
-
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
             if (gamepad1.options) {
                 imu.resetYaw();
             }
