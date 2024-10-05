b7ff652f1e5fcebeef55e400c8a85ae59e99f768
Sid. S <siddharth.shailesh@icloud.com>
2024-10-04 20:40:02 -0400

    LinearSlides Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTrain.java
@@ -13,7 +13,6 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import java.util.ArrayList;
 
 @TeleOp
@@ -23,6 +22,7 @@ public class MecanumDriveTrain extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
+
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
@@ -31,19 +31,24 @@ public class MecanumDriveTrain extends LinearOpMode {
         Servo clawServo1 = hardwareMap.get(Servo.class, "ClawServo1");
         Servo clawServo2 = hardwareMap.get(Servo.class, "ClawServo2");
 
+        clawServo1.setPosition(0.40);
+        clawServo2.setPosition(0.65);
+
+        double position = 0;
+
         ElapsedTime timeSinceRightBumperPressed = new ElapsedTime();
 
         boolean claw_state = false; // false as open, true as closed
 
-        double claw1_position = clawServo1.getPosition();
-        double claw2_position = clawServo2.getPosition();
+        //double claw1_position = 0.5;
+        //double claw2_position = 0.5;
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
@@ -53,14 +58,16 @@ public class MecanumDriveTrain extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
+
         waitForStart();
+
+
 
         while (opModeIsActive()) {
 
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
-
 
 
 
@@ -68,20 +75,20 @@ public class MecanumDriveTrain extends LinearOpMode {
                 if(timeSinceRightBumperPressed.milliseconds() > 500) {
                     timeSinceRightBumperPressed.reset();
                     claw_state = !claw_state;
-
                 }
 
 
             }
 
             if(claw_state) {
-              clawServo1.setPosition(claw1_position - 0.3);
-              clawServo2.setPosition(claw2_position + 0.3);
+              clawServo1.setPosition(0.05);
+              clawServo2.setPosition(1);
             }
             else {
-              clawServo1.setPosition(claw1_position);
-              clawServo2.setPosition(claw2_position);
+              clawServo1.setPosition(0.40);
+              clawServo2.setPosition(0.65);
             }
+
 
             if (gamepad1.options) {
                 imu.resetYaw();
@@ -108,6 +115,8 @@ public class MecanumDriveTrain extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+
         }
     }
 }
