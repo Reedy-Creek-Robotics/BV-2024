1e0dbba7aaebf5a79f4d2a6d69656f02d8b4aa87
akshay <sideswifter2010@gmail.com>
2024-10-05 20:27:48 -0400

    Akshay Auto WITH FUNCTIONS and working code with tank turn and stop function, with some things renamed and comments to explain code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.LruCache;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -28,34 +30,63 @@ public class AutoCode extends LinearOpMode {
         Servo clawServo1 = hardwareMap.get(Servo.class, "ClawServo1");
         Servo clawServo2 = hardwareMap.get(Servo.class, "ClawServo2");
 
-        class innerClass {
-            public void innerMethod(double power, int wait){
-                frontLeftMotor.setPower(power);
-                frontRightMotor.setPower(power);
-                backLeftMotor.setPower(power);
-                backRightMotor.setPower(power);
-                sleep(wait);
+
+        // class with all movement functions
+        class Movement {
+
+            // stop function/method which stops all motors
+            public void Stop(){
                 frontLeftMotor.setPower(0);
                 frontRightMotor.setPower(0);
                 backLeftMotor.setPower(0);
                 backRightMotor.setPower(0);
             }
+
+
+            // moves the motors at a speed for a set time and stops the motors
+            public void Forward(double power, int wait) {
+                frontLeftMotor.setPower(power);
+                frontRightMotor.setPower(power);
+                backLeftMotor.setPower(power);
+                backRightMotor.setPower(power);
+                sleep(wait);
+                stop();
+            }
+
+
+            // takes 2 inputs, the turning speed and waiting time to stop
+            public void TankTurn(double TurnVal, int wait){
+                frontLeftMotor.setPower(TurnVal);
+                frontRightMotor.setPower(TurnVal);
+                backLeftMotor.setPower(TurnVal);
+                backRightMotor.setPower(TurnVal);
+                sleep(wait);
+                stop();
+            }
         }
 
-        innerClass inner = new innerClass();
+        Movement inner = new Movement();
 
 
 
 
 
         waitForStart();
-        // for some reason does not add telemetry data idk why
+
+
+
         telemetry.addData("running","True");
         telemetry.update();
-        inner.innerMethod(0.3, 3000);
+
+        /*
+         calls a inner class which is movement and gets the functions gives inputs and runs,
+         this is where our main auto code will go
+         */
+        inner.Forward(0.6, 3000);
+        inner.TankTurn(0.5,500);
 
 
-        // goes forward with 20% power for half a second
+
 
 
     }
